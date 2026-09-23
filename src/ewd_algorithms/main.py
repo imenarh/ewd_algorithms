e10b360c2f64bbe738571da8ae189bc705e87c7d
Herve Rwigema <h.rwigema@alustudent.com>
2026-09-23 10:52:10 +0200

    implementing the linear search and random number functions

--- src/ewd_algorithms/main.py
+++ src/ewd_algorithms/main.py
@@ -1,9 +1,22 @@
-def linear_search(data):
-    
-    pass
+import random
+from ewd_algorithms.drivers import drivers
+
+def gen_target():
+    number= random.randint(1, 10000)
+    padded = f"DRV{number:05d}"
+    return padded
+
+def linear_search(data, target):
+    for driver_id, name in data.items():
+        if driver_id == target:
+            return name, driver_id
+    return None
 
 def binary_search(data):
     pass
 
 def hashmap(data):
     pass
+
+target = gen_target()
+print(linear_search(drivers, target))
